9ed228ca073484c5534c7648fe93ecc5daa445cf
bdambrosio <bruce.dambrosio@gmail.com>
2021-06-15 09:27:24 -0700

    misc updates pvScrape new scale parameters, first draft of pvDisplayMQTT2.0 for MQTT and Pi0 python

--- pvMonitorSocket.py
+++ pvMonitorSocket.py
@@ -34,95 +34,16 @@ i2c = machine.I2C(scl=Pin(5), sda=Pin(4) )   # create and init as a master
 # 1115 i2c addr = 72 -> addr pin to gnd, 5 -> max gain
 aadc = ads1x15.ADS1115(i2c,72,5) 
 # Display driver
-oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=60)
-oled.text('PV Monitor', 10, 0)
+oled_present=True
+try:
+    oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c, addr=60)
+    oled.text('PV Monitor', 10, 0)
+    
+except:
+    oled_present=False
+    
 _time = rtc.datetime()
-
-oled.fill_rect(0, 16, 128, 48, 0)
-oled.text(str(_time[0]),  5, 16)
-oled.text(str(_time[1]), 42, 16)
-oled.text(str(_time[2]), 60, 16)
-oled.text(str(_time[4]), 80, 16)
-oled.text(str(_time[5]), 100, 16)
-              
-oled.show()
-
-#get network object so we can check connection status
-sta_if = network.WLAN(network.STA_IF);
-
-# start socket service
-s = socket.socket()
-addr = socket.getaddrinfo('0.0.0.0', 1884)[0][-1]
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(addr)
-s.listen(5)
-
-print('listening on', addr)
-
-# json structures for publishing values
-# publish for two locations, comment out as appropriate
-#jsonVolts = dict(units='pv.battery.output.voltage')
-#jsonAmps = dict(units='pv.battery.output.current')
-jsonVolts = dict(units='pv.battery.input.voltage')
-jsonAmps = dict(units='pv.battery.input.current')
-
-measurements = {}
-measurements['voltage'] = jsonVolts
-measurements['current'] = jsonAmps
-
-default_scale = {'voltage':398.8, 'current':1454.5}
-#micropython.mem_info()
-v1 = aadc.read(0,2)
-utime.sleep_ms(200)
-i1 = aadc.read(0,0,1)
-utime.sleep_ms(200)
-v2 = aadc.read(0,2)
-utime.sleep_ms(200)
-i2 = aadc.read(0,0,1)
-    
-print ("v1", v1, "i1",i1)
-print ("v2", v2, "i2",i2)
-
-while True:
-    cl, addr = s.accept()
-    try:
-        _jsonScale = cl.recv(256)
-        # print("received", _jsonScale)
-        _scale = json.loads(_jsonScale)
-    except:
-        print("failed to get scale")
-        _scale = default_scale
-    # denoise: take two readings a sec apart and average
-    _v1 = aadc.read(0,2)
-    utime.sleep_ms(200)
-    _i1 = aadc.read(0,0,1)
-    utime.sleep_ms(200)
-    _v2 = aadc.read(0,2)
-    utime.sleep_ms(200)
-    _i2 = aadc.read(0,0,1)
-    v = (_v1+_v2)/2
-    i = (_i1+_i2)/2
-    
-    #scale into volts/amps
-    volts =  aadc.raw_to_v(v)*_scale['voltage']
-    amps =  aadc.raw_to_v(i) * _scale['current']
-    #print(v, i, volts, amps)
-    #publish readings via mqtt and store in table
-    jsonVolts['value'] = volts
-    jsonAmps['value'] = amps
-    # At this point in the code you must consider how to handle
-    # connection errors.  And how often to resume the connection.
-    if not sta_if.isconnected():
-      print("network connection lost")
-      while not sta_if.connect():
-        # If the connection is successful, the is_conn_issue
-        # method will not return a connection error.
-        print("reconnecting to network")
-        sta_if.connect()
-    cl.send(json.dumps(measurements))
-    cl.close()
-
-    _time = rtc.datetime()
+if oled_present:
     oled.fill_rect(0, 16, 128, 48, 0)
     oled.text(str(_time[0]),  5, 16)
     oled.text(str(_time[1]), 42, 16)
@@ -130,10 +51,114 @@ while True:
     oled.text(str(_time[4]), 80, 16)
     oled.text(str(_time[5]), 100, 16)
               
-    oled.text("V ",   4,32)
-    oled.text(str(int(volts*10)), 24, 32)
-    oled.text("I ",   66,32)
-    oled.text(str(int(amps*10)),  80, 32)
     oled.show()
+
+#get network object so we can check connection status
+wlan = network.WLAN(network.STA_IF);
+ipaddr = wlan.ifconfig()[0]
+
+# start socket service
+sock_addr=socket.getaddrinfo('0.0.0.0', 1884)[0][-1]
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(sock_addr)
+s.listen(5)
+
+print('listening on', ipaddr)
+
+
+measurements = {}
+jsonVolts = {'value':-1.0}
+jsonAmps = {'value':-1.0}
+measurements['voltage'] = jsonVolts
+measurements['current'] = jsonAmps
+
+default_scale = {'v_scale':398.8, 'v_offset':0.0, 'i_scale':1454.5, 'i_offset':0.0}
+#micropython.mem_info()
+
+v1 = -1
+v2 = -1
+i1 = -1
+i2 = -1
+ads_present = True
+
+try:
+    v1 = aadc.read(0,2)
+except:
+    ads_present = False
+
+if ads_present: # absent for test sensor for testing pvScrape comm
+    utime.sleep_ms(200)
+    i1 = aadc.read(0,0,1)
+    utime.sleep_ms(200)
+    v2 = aadc.read(0,2)
+    utime.sleep_ms(200)
+    i2 = aadc.read(0,0,1)
+    print ("v1", v1, "i1",i1)
+    print ("v2", v2, "i2",i2)
+
+while True:
+    try:
+        cl, addr = s.accept()
+        try:
+            _jsonScale = cl.recv(256)
+            # print("received", _jsonScale)
+            _scale = json.loads(_jsonScale)
+        except:
+            print("failed to get scale")
+            _scale = default_scale
+        v = -1.0
+        i = -1.0
+        # denoise: take two readings a sec apart and average
+        if ads_present:
+            _v1 = aadc.read(0,2)
+            utime.sleep_ms(200)
+            _i1 = aadc.read(0,0,1)
+            utime.sleep_ms(200)
+            _v2 = aadc.read(0,2)
+            utime.sleep_ms(200)
+            _i2 = aadc.read(0,0,1)
+            v = (_v1+_v2)/2
+            i = (_i1+_i2)/2
+    
+        #scale into volts/amps
+        volts =  aadc.raw_to_v(v) * _scale['v_scale'] + _scale['v_offset']
+        amps =  aadc.raw_to_v(i) * _scale['i_scale'] + _scale['i_offset']
+        if not oled_present:
+            print(v, i, volts, amps)
+        # for debugging, comment out before deploy
+        print(v, i, volts, amps)
+        #publish readings via mqtt and store in table
+        jsonVolts['value'] = volts
+        jsonAmps['value'] = amps
+        # At this point in the code you must consider how to handle
+        # connection errors.  And how often to resume the connection.
+        if not wlan.isconnected():
+            print("network connection lost")
+            while not wlan.connect():
+                # If the connection is successful, the is_conn_issue
+                # method will not return a connection error.
+                print("reconnecting to network")
+                wlan.connect()
+        cl.send(json.dumps(measurements))
+    except:
+        try:
+            cl.close()
+        except:
+            pass
+    _time = rtc.datetime()
+    if oled_present:
+        oled.fill_rect(0, 16, 128, 48, 0)
+        oled.text(str(_time[0]),  5, 16)
+        oled.text(str(_time[1]), 42, 16)
+        oled.text(str(_time[2]), 60, 16)
+        oled.text(str(_time[4]), 80, 16)
+        oled.text(str(_time[5]), 100, 16)
+        
+        oled.text("V ",   4,32)
+        oled.text(str(int(volts*10)), 24, 32)
+        oled.text("I ",   66,32)
+        oled.text(str(int(amps*10)),  80, 32)
+        oled.show()
     gc.collect()
     # print('Initial free: {} allocated: {}'.format(gc.mem_free(), gc.mem_alloc()))
---
+++ pvScrape.py
@@ -0,0 +1,129 @@
+import time as utime
+import json
+import socket
+import time
+import paho.mqtt.client as mqtt
+import json
+import sqlite3
+import sys
+db = sqlite3.connect('pv.db')
+hourly_total = {}
+hourly_count = {}
+last_db_update_time = {}
+
+def update_db(topic, value):
+    global hourly_total, hourly_count, last_db_update_time, db
+
+    if not topic in hourly_total.keys():
+        hourly_total[topic] = 0.0 # start new accumulator
+        hourly_count[topic] = 0
+        last_db_update_time[topic] = time.time() - 3601 # because this measurement not in last update
+    
+    hourly_total[topic] += value
+    hourly_count[topic] += 1
+    int_time = int(time.time())
+    if int_time - last_db_update_time[topic] > 3600: # more than 1 hr
+        try:
+            hour_value = hourly_total[topic]/hourly_count[topic]
+            new_row="INSERT INTO "+"'"+topic[3:]+"'"+" ('time', 'value') VALUES ({0}, {1:6.2f})".format(int_time, hour_value)
+            print(new_row)
+            cursor=db.cursor()
+            rc=cursor.execute(new_row)
+            db.commit()
+            cursor.close()
+        except:
+            print ("Error updating db")
+        #note we reset regardless of db update success, so that eventual success will have one hr total
+        hourly_total[topic] = 0.0
+        hourly_count[topic] = 0
+        last_db_update_time[topic] = int_time
+
+#setup to publish to mosquitto broker
+def new_msg():
+    print ("new msg")
+    
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("MQTT connect success")
+    else:
+        print(f"MQTT connect fail with code {rc}")
+
+def on_publish(client,userdata,result):             #create function for callback
+    #print("data published ", result)
+    pass
+
+client = mqtt.Client() 
+client.on_connect = on_connect
+client.on_message = new_msg
+client.on_publish = on_publish
+client.username_pw_set(username='mosq', password='1947nw')
+client.connect("127.0.0.1", 1883, 60) 
+
+battery_input_scale = {'v_scale':398.8, 'v_offset':0.0,'i_scale':111.0, 'i_offset':0.01}
+battery_input_prefix = 'pv.battery.input.'
+battery_input_ipaddr = '192.168.1.110'
+
+battery_output_scale = {'v_scale':398.8, 'v_offset':0.0,'i_scale':950.0, 'i_offset':0.0}
+battery_output_prefix ='pv.battery.output.'
+battery_output_ipaddr =  '192.168.1.148'
+
+battery_test_scale = {'v_scale':398.8, 'v_offset':0.0,'i_scale':1425.0, 'i_offset':0.0}
+battery_test_prefix = 'pv.battery.test.'
+battery_test_ipaddr =  '192.168.1.110'
+
+def process_sensor(ipaddr, prefix, scale):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            print("connecting to ", ipaddr)
+            s.connect((ipaddr, 1884)) 
+            s.sendall(bytearray(json.dumps(scale), 'utf8'))
+        except OSError:
+            print ('Socket connect failed! Loop up and try socket again')
+            utime.sleep( 2.0)
+            return
+        
+        try:
+            msg=s.recv(1024)
+        except OSError:
+            print ('Socket timeout, loop and try recv() again')
+            utime.sleep( 5.0)
+            return
+        
+        # print ('received', str(msg, 'utf8'))
+        try:
+            measurements = json.loads(str(msg, 'utf8'))
+        except:
+            print("json parse failure: ",ipaddr)
+            return
+        
+        scaled_value = 0.0
+        for item,data in measurements.items():
+            try:
+                value = data['value']
+                scaled_value = value
+                label = prefix
+                if 'voltage' in item:
+                    label=prefix+'voltage'
+                elif 'current' in item:
+                    label=prefix+'current'
+                print(label, value)
+                try:
+                    rc = client.publish(label, value)
+                except:
+                    print("error posting measurement, skipping")
+                update_db(label, value)
+            except:
+                print ("error processing ", label)
+
+while True:
+    # print("checking connection")
+    utime.sleep(5)
+    # try openning connection to pv monitor hardware
+    if len(sys.argv) < 2:
+        process_sensor(battery_input_ipaddr, battery_input_prefix, battery_input_scale)
+        process_sensor(battery_output_ipaddr, battery_output_prefix, battery_output_scale)
+    elif  sys.argv[1] == 'test':
+        print("processing test sensor")
+        process_sensor(sys.argv[2], battery_test_prefix, battery_test_scale)
+        
+
