9b6f56c70748af45c0cdad4f77cfe0b07157cb8f
bdambrosio <bruce.dambrosio@gmail.com>
2021-06-22 18:03:06 +0100

    reformat display to add date/time and one decimal place on values

--- pvDisplayPi0SimpleGUI.py
+++ pvDisplayPi0SimpleGUI.py
@@ -1,19 +1,26 @@
 # pv display for Pi0 using python3 and PySimpleGUI
-
 import time
+from time import ctime
+from datetime import datetime
+import pytz
 import json
 import paho.mqtt.client as mqtt
 import threading
 
-Vin = ' 0.0'
-Vout = ' 0.0'
-Iin = ' 0.0'
-Iout = ' 0.0'
+Vin = ' 00.0'
+Vout = ' 00.0'
+Iin = ' 00.0'
+Iout = ' 00.0'
+ptz = pytz.timezone('America/Los_Angeles')
+utc = pytz.timezone('UTC')
+now = utc.localize(datetime.utcnow())
+Time = str(now.astimezone(ptz))[:-13]
+
 import PySimpleGUI as sg
 sg.theme('DarkAmber')   # Add a little color to your windows
 sg.set_options(font=('Helvetica', 14))
 # All the stuff inside your window. This is the PSG magic code compactor...
-layout = [  [sg.Text('PV Monitor')],
+layout = [  [sg.Text('PV Monitor'), sg.Text(Time, key='-time-')],
             [sg.Text('Battery In   V: '), sg.Text(Vin, key='-Vin-'),
              sg.Text(' I: '), sg.Text(Iin, key='-Iin-')],
             [sg.Text('Battery Out  V: '), sg.Text(Vout, key='-Vout-'),
@@ -27,20 +34,24 @@ def new_measurement(client, userdata, msg):
     topic = msg.topic
     measurement = json.loads(msg.payload)
     #print(topic, measurement)
+    now = utc.localize(datetime.utcnow())
+    Time = str(now.astimezone(ptz))[:-13]
+    window['-time-'].update(Time)
     if 'output' in topic:
         if 'current' in topic:
-            Iout = " {0:4.2f}".format(measurement)
+            Iout = " {0:5.2f}".format(measurement)
             window['-Iout-'].update(Iout)
+            print(Iout)
         else:
-            Vout = " {0:4.2f}".format(measurement)
+            Vout = " {0:5.2f}".format(measurement)
             window['-Vout-'].update(Vout)
         
     elif 'input' in topic:
         if 'current' in topic:
-            Iin = " {0:4.2f}".format(measurement)
+            Iin = " {0:5.2f}".format(measurement)
             window['-Iin-'].update(Iin)
         else:
-            Vin = " {0:4.2f}".format(measurement)
+            Vin = " {0:5.2f}".format(measurement)
             window['-Vin-'].update(Vin)
     
 # start mqtt client
